97e9c75d67ac3a2a80a0f5239cc7efd26fb38172
duardoqueiroz <eduardoldq1@gmail.com>
2021-09-09 20:25:36 -0300

    feat: user creation service

---
+++ src/service/CreateUserService.ts
@@ -0,0 +1,41 @@
+import { UserRepositories } from '../repositories/UserRepositories'
+import { getCustomRepository } from 'typeorm'
+import { hash } from 'bcryptjs'
+interface IUserRequest {
+  name: string
+  email: string
+  admin?: boolean
+  password: string
+}
+
+class CreateUserService {
+  async execute({ name, email, admin = false, password }: IUserRequest) {
+    const userRepositories = getCustomRepository(UserRepositories)
+
+    if (!email) {
+      throw new Error('Email incorrect')
+    }
+
+    const alreadyExists = await userRepositories.findOne({
+      email
+    })
+
+    if (alreadyExists) {
+      throw new Error('User already exists')
+    }
+
+    const passwordHash = await hash(password, 8)
+
+    const user = userRepositories.create({
+      name,
+      email,
+      admin,
+      password: passwordHash
+    })
+    await userRepositories.save(user)
+
+    return user
+  }
+}
+
+export { CreateUserService }
