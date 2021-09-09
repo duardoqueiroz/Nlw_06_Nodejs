5f8d0c6093e31fa0aecbf75fb83f18730dcf7515
duardoqueiroz <eduardoldq1@gmail.com>
2021-09-09 20:10:42 -0300

    feat: control to list the compliments sent

---
+++ src/controllers/ListUserSenderComplimentsController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express'
+import { ListUserSenderComplimentsService } from '../service/ListUserSenderComplimentsService'
+
+export class ListUserSenderComplimentsController {
+  async handle(req: Request, res: Response) {
+    const { user_id } = req
+
+    const listUserSenderComplimentsService =
+      new ListUserSenderComplimentsService()
+
+    const compliments = await listUserSenderComplimentsService.execute(user_id)
+
+    return res.json(compliments)
+  }
+}
